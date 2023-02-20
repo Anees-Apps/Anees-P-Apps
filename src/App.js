967ab0f8001546b6cb9e82aa9ceafaab5d211f07
Anees-Apps <aneesurrehman236@gmail.com>
2023-02-20 07:35:21 +0500

    Init Basic App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Quiz from './Components/Quiz'; 
+// import {QuizData} from './Data/QuizData'
+import './App.css';
+
+function App() {
+  return (
+
+    <Quiz/>
+  )
+}
+
+export default App;
